b948fb2e6ec301eacb0c31950deff66de87caa93
tibix <tiberiu.purcel@gmail.com>
2023-10-27 23:50:56 +0300

    Creating first draft of the DB

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
-	phpinfo();
+	$title = "Proiect pentru Proiectare WEB";
+	$authors = ["Ovidiu", "Ionut", "Tiberiu"];
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Proiectare WEB</title>
+</head>
+<body>
+	<h1><?php echo $title ?></h1>
+	<ul>
+		<?php
+			foreach($authors as $author) {
+				echo "<li> {$author} </li>";
+			}
+		?>
+	</ul>
+</body>
+</html>
